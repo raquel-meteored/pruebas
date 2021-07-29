0a2a6b510ed1fe28bed824fcb89b425504772e84
Raquel <raquel@meteored.com>
2021-07-29 14:23:08 +0200

    Se incluye gráfica de acierto vs fallos de la situación actual

--- check_synop_prec.sh
+++ check_synop_prec.sh
@@ -4,7 +4,7 @@ set -ex
 
 DIR_BASE=/home/raquel/repos/pruebas
 
-i=1; while printf '%d' "$((i++))"; (( i <= 100)); do
+i=1; while printf '%d' "$((i++))"; (( i <= 1000)); do
 
   ntiempo=$((1 + $RANDOM % 450000))
   lat=$( mysql -uogimet plocales -se "select latitud from puntos where id_tiempo='$ntiempo'" )
@@ -21,8 +21,8 @@ i=1; while printf '%d' "$((i++))"; (( i <= 100)); do
   fi
 
 
-  curl -m 10 --connect-timeout 30  'http://localhost/cgi-bin/otf_synop2?latitud='$lat'&longitud='$lon'&radio=1000' -o tmp.json
-  jq .[] tmp.json >/dev/null
+  curl -m 20 --connect-timeout 60  'http://localhost/cgi-bin/otf_synop2?latitud='$lat'&longitud='$lon'&radio=1000' -o tmp.json
+  jq .[] tmp.json >/dev/null 2>&1
   check=$?
 
   if [[ $check != 0 ]]; then
@@ -31,6 +31,7 @@ i=1; while printf '%d' "$((i++))"; (( i <= 100)); do
   fi
 
   if [[ $(jq '.ok' tmp.json) != "false" ]]; then
+
       dist=$(jq '.Distancia' tmp.json )
       tiempo=$(jq '.Tiempo' tmp.json )
       ult_hora=$(jq '.synop[].hora' tmp.json | tail -1 | tr "\"" " " )
@@ -41,7 +42,6 @@ i=1; while printf '%d' "$((i++))"; (( i <= 100)); do
       jq '.synop[].hora' tmp.json > tmp1
 
       paste tmp1 tmp2 tmp3 | tr "\"" " " >> chequeo_situacion_actual_PR.txt
-
 
   fi
 
@@ -62,8 +62,10 @@ done
 for hora in $(seq -w 1 23);do
 
    ndato1=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora' && $2==1) print $0}' | wc -l)
-
    ndato3=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora' && $2==03) print $0}' | wc -l)
+
+   nfallos=$(cat ${DIR_BASE}/chequeo_situacion_actual_false.txt | awk '{if ($1=='$hora') print $0}' | wc -l )
+   naciertos=$(cat ${DIR_BASE}/chequeo_situacion_actual_dist.txt | awk '{if ($1=='$hora') print $0}' | wc -l )
 
    if [[ -z "$ndato1" ]];then
        ndato1=0
@@ -73,23 +75,28 @@ for hora in $(seq -w 1 23);do
        ndato3=0
    fi
 
-   echo $hora 1 $ndato1 3 $ndato3  >> tmp2
+   echo $hora 1 $ndato1 3 $ndato3  >> tmp-prec
+   echo $hora $nfallos $aciertos >> tmp-fallos
 
 done
-awk '{if (NF==3) print $0}' tmp1 > ${DIR_BASE}/chequeo_precip_01.txt
-awk '{if (NF==3) print $0}' tmp2 > ${DIR_BASE}/chequeo_precip_03.txt
-
-#rm tmp*
+awk '{if (NF==5) print $0}' tmp-prec > ${DIR_BASE}/chequeo_PR.txt
+awk '{if (NF==3) print $0}' tmp-fallos > ${DIR_BASE}/chequeo_fallos.txt
+rm tmp-prec tmp-fallos
 
 cat>inR2<<EOF
 sitact=read.table("${DIR_BASE}/chequeo_situacion_actual_dist.txt")
 prec=read.table("${DIR_BASE}/chequeo_situacion_actual_PR.txt")
+prechoras=read.table("${DIR_BASE}/chequeo_PR.txt")
+fallos=read.table("${DIR_BASE}/chequeo_fallos.txt")
 
-pr1h=subset(prec,prec[,2]=1)
-pr3h=subset(prec,prec[,2]=3)
+pr1h=subset(prec,prec[,2]==1)
+pr3h=subset(prec,prec[,2]==3)
 
 n1h=length(pr1h)
 n3h=length(pr3h)
+
+pdf("${DIR_BASE}/horas_vs_fallos.pdf")
+plot(fallos[,1], fallos[,2], type="b")
 
 pdf("${DIR_BASE}/hist_dist.pdf")
 hist(sitact[,2], main="Distacia synop petición", xlab="Distacia (m)")
@@ -97,10 +104,15 @@ hist(sitact[,2], main="Distacia synop petición", xlab="Distacia (m)")
 pdf("${DIR_BASE}/hist_tiempo.pdf")
 hist(sitact[,3], main="Tiempo synop petición", xlab="Tiempo (s)")
 
-
-
+pdf("${DIR_BASE}/horas_vs_estprec.pdf")
+lim=max(prechoras[,3],prechoras[,5])
+plot(prechoras[,1],prechoras[,3],type="b", col="blue", ylim=c(0,lim), xlab="Horas", ylab="# estaciones", main="Estacion con precip. horaria y trihoraria")
+lines(prechoras[,5],type="b",col="tomato")
+legend("topright",lty=c(1,1),col=c("blue","tomato"),c("1h","3h"))
 
 dev.off()
 q()
 EOF
 R CMD BATCH inR2
+
+rm tmp*
