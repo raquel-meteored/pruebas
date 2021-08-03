3464294fc568346aa901efbb1f02dd3c4a50f79a
Raquel <raquel@meteored.com>
2021-08-03 15:16:28 +0200

    Se añaden mejoras al script de chequeo de la situación actual

--- check_synop_prec.sh
+++ check_synop_prec.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#Buscamos estaciones con diferentes acumulados de precipitación
+#Comprobaciones sobre los jsons de situación actual
 set -ex
 
 DIR_BASE=/home/raquel/repos/pruebas
 
-i=1; while printf '%d' "$((i++))"; (( i <= 1000)); do
+i=1; while printf '%d' "$((i++))"; (( i <= 100)); do
 
+  #Cogemos aleatoriamente localidades detro de la BD plocales
   ntiempo=$((1 + $RANDOM % 450000))
   lat=$( mysql -uogimet plocales -se "select latitud from puntos where id_tiempo='$ntiempo'" )
   long=$( mysql -uogimet plocales -se "select longitud from puntos where id_tiempo='$ntiempo'" )
@@ -16,20 +17,23 @@ i=1; while printf '%d' "$((i++))"; (( i <= 1000)); do
     lon="$long"
   fi
 
+  #Hay estaciones con id pero sin coord. Esto es un poco raro.TODO
   if [[ -z "$lat" ]]; then
     continue
   fi
 
-
-  curl -m 20 --connect-timeout 60  'http://localhost/cgi-bin/otf_synop2?latitud='$lat'&longitud='$lon'&radio=1000' -o tmp.json
+  #Pedimos situación actual
+  curl -m 20 --connect-timeout 60  'http://localhost/cgi-bin/otf_synop?latitud='$lat'&longitud='$lon'&radio=1000&check=true' -o tmp.json
   jq .[] tmp.json >/dev/null 2>&1
   check=$?
 
+  #Guardamos los json que jq no entiende. Esto no funciona bien, se para. TODO
   if [[ $check != 0 ]]; then
      mv tmp.json error_"$lon"_"$lat".json
      continue
   fi
 
+  #Extraemos varibles de interés para el análisis
   if [[ $(jq '.ok' tmp.json) != "false" ]]; then
 
       dist=$(jq '.Distancia' tmp.json )
@@ -37,17 +41,17 @@ i=1; while printf '%d' "$((i++))"; (( i <= 1000)); do
       ult_hora=$(jq '.synop[].hora' tmp.json | tail -1 | tr "\"" " " )
       name=$(jq '.nombre' tmp.json )
 
-      jq '.synop[].precipitacion.valor' tmp.json > tmp3
-      jq '.synop[].tr' tmp.json > tmp2
       jq '.synop[].hora' tmp.json > tmp1
+      jq '.synop[].tr' tmp.json > tmp2
+      jq '.synop[].precipitacion.valor' tmp.json > tmp3
 
       paste tmp1 tmp2 tmp3 | tr "\"" " " >> chequeo_situacion_actual_PR.txt
-
   fi
 
+  #Analizamos el porcentaje de estaciones con dato
   if [[ $(jq '.ok' tmp.json) == "false" ]]; then
 
-    echo false $lat $lon  >> ${DIR_BASE}/chequeo_situacion_actual_false.txt
+    echo $(date -u '+%H') false $lat $lon  >> ${DIR_BASE}/chequeo_situacion_actual_false.txt
 
   else
 
@@ -59,10 +63,12 @@ i=1; while printf '%d' "$((i++))"; (( i <= 1000)); do
 
 done
 
+
+#Analisis por horas
 for hora in $(seq -w 1 23);do
 
    ndato1=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora' && $2==1) print $0}' | wc -l)
-   ndato3=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora' && $2==03) print $0}' | wc -l)
+   ndato3=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora' && $2==3) print $0}' | wc -l)
 
    nfallos=$(cat ${DIR_BASE}/chequeo_situacion_actual_false.txt | awk '{if ($1=='$hora') print $0}' | wc -l )
    naciertos=$(cat ${DIR_BASE}/chequeo_situacion_actual_dist.txt | awk '{if ($1=='$hora') print $0}' | wc -l )
@@ -76,13 +82,16 @@ for hora in $(seq -w 1 23);do
    fi
 
    echo $hora 1 $ndato1 3 $ndato3  >> tmp-prec
-   echo $hora $nfallos $aciertos >> tmp-fallos
+   echo $hora $nfallos $naciertos >> tmp-fallos
 
 done
+
+
 awk '{if (NF==5) print $0}' tmp-prec > ${DIR_BASE}/chequeo_PR.txt
 awk '{if (NF==3) print $0}' tmp-fallos > ${DIR_BASE}/chequeo_fallos.txt
 rm tmp-prec tmp-fallos
 
+#Pintamos el análisis con R
 cat>inR2<<EOF
 sitact=read.table("${DIR_BASE}/chequeo_situacion_actual_dist.txt")
 prec=read.table("${DIR_BASE}/chequeo_situacion_actual_PR.txt")
@@ -92,17 +101,18 @@ fallos=read.table("${DIR_BASE}/chequeo_fallos.txt")
 pr1h=subset(prec,prec[,2]==1)
 pr3h=subset(prec,prec[,2]==3)
 
-n1h=length(pr1h)
-n3h=length(pr3h)
-
-pdf("${DIR_BASE}/horas_vs_fallos.pdf")
-plot(fallos[,1], fallos[,2], type="b")
 
 pdf("${DIR_BASE}/hist_dist.pdf")
 hist(sitact[,2], main="Distacia synop petición", xlab="Distacia (m)")
 
 pdf("${DIR_BASE}/hist_tiempo.pdf")
 hist(sitact[,3], main="Tiempo synop petición", xlab="Tiempo (s)")
+
+pdf("${DIR_BASE}/horas_vs_fallos.pdf")
+plot(fallos[,1], fallos[,3], type="b", xlab="Horas", ylab="# estaciones",col="blue", main="Estaciones con observaciones vs vacías")
+lines(fallos[,2],type="b",col="tomato")
+legend("topright",lty=c(1,1),col=c("blue","tomato"),c("con datos","vacías"),title="Estaciones")
+
 
 pdf("${DIR_BASE}/horas_vs_estprec.pdf")
 lim=max(prechoras[,3],prechoras[,5])
