d639abff355b99cd184b4b3bc8f6e97c245bf682
Raquel <raquel@meteored.com>
2021-08-05 09:19:47 +0200

    Se incluyen paths y se arreglan bugs

--- check_synop_prec.sh
+++ check_synop_prec.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Comprobaciones sobre los jsons de situación actual
-set -ex
 
 DIR_BASE=/home/raquel/repos/pruebas
 
@@ -23,13 +22,13 @@ i=1; while printf '%d' "$((i++))"; (( i <= 100)); do
   fi
 
   #Pedimos situación actual
-  curl -m 20 --connect-timeout 60  'http://localhost/cgi-bin/otf_synop?latitud='$lat'&longitud='$lon'&radio=1000&check=true' -o tmp.json
+  curl -s -m 20 --connect-timeout 60  'http://localhost/cgi-bin/otf_synop?latitud='$lat'&longitud='$lon'&radio=1000&check=true' -o tmp.json
   jq .[] tmp.json >/dev/null 2>&1
   check=$?
 
   #Guardamos los json que jq no entiende. Esto no funciona bien, se para. TODO
   if [[ $check != 0 ]]; then
-     mv tmp.json error_"$lon"_"$lat".json
+     mv tmp.json ${DIR_BASE}/error_"$lon"_"$lat".json
      continue
   fi
 
@@ -45,30 +44,23 @@ i=1; while printf '%d' "$((i++))"; (( i <= 100)); do
       jq '.synop[].tr' tmp.json > tmp2
       jq '.synop[].precipitacion.valor' tmp.json > tmp3
 
-      paste tmp1 tmp2 tmp3 | tr "\"" " " >> chequeo_situacion_actual_PR.txt
-  fi
-
-  #Analizamos el porcentaje de estaciones con dato
-  if [[ $(jq '.ok' tmp.json) == "false" ]]; then
-
-    echo $(date -u '+%H') false $lat $lon  >> ${DIR_BASE}/chequeo_situacion_actual_false.txt
+      paste tmp1 tmp2 tmp3 | tr "\"" " " >> ${DIR_BASE}/chequeo_situacion_actual_PR.txt
+      echo  ${ult_hora} $dist $tiempo $lat $lon $name >> ${DIR_BASE}/chequeo_situacion_actual_dist.txt
 
   else
-
-    echo  ${ult_hora} $dist $tiempo $lat $lon $name >> ${DIR_BASE}/chequeo_situacion_actual_dist.txt
-
+    echo $(date -u '+%H') false $lat $lon  >> ${DIR_BASE}/chequeo_situacion_actual_false.txt
   fi
 
-  #mv tmp.json kk_"$i".json
 
 done
 
 
 #Analisis por horas
-for hora in $(seq -w 1 23);do
+for hora in $(seq -w 0 23);do
 
    ndato1=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora' && $2==1) print $0}' | wc -l)
    ndato3=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora' && $2==3) print $0}' | wc -l)
+   ndato=$(cat ${DIR_BASE}/chequeo_situacion_actual_PR.txt | awk '{if ($1=='$hora') print $0}' | wc -l)
 
    nfallos=$(cat ${DIR_BASE}/chequeo_situacion_actual_false.txt | awk '{if ($1=='$hora') print $0}' | wc -l )
    naciertos=$(cat ${DIR_BASE}/chequeo_situacion_actual_dist.txt | awk '{if ($1=='$hora') print $0}' | wc -l )
@@ -81,43 +73,40 @@ for hora in $(seq -w 1 23);do
        ndato3=0
    fi
 
-   echo $hora 1 $ndato1 3 $ndato3  >> tmp-prec
+   echo $hora 1 $ndato1 3 $ndato3 $ndato >> tmp-prec
    echo $hora $nfallos $naciertos >> tmp-fallos
 
 done
 
 
-awk '{if (NF==5) print $0}' tmp-prec > ${DIR_BASE}/chequeo_PR.txt
+awk '{if (NF==6) print $0}' tmp-prec > ${DIR_BASE}/chequeo_PR.txt
 awk '{if (NF==3) print $0}' tmp-fallos > ${DIR_BASE}/chequeo_fallos.txt
 rm tmp-prec tmp-fallos
 
 #Pintamos el análisis con R
 cat>inR2<<EOF
 sitact=read.table("${DIR_BASE}/chequeo_situacion_actual_dist.txt")
-prec=read.table("${DIR_BASE}/chequeo_situacion_actual_PR.txt")
 prechoras=read.table("${DIR_BASE}/chequeo_PR.txt")
 fallos=read.table("${DIR_BASE}/chequeo_fallos.txt")
 
-pr1h=subset(prec,prec[,2]==1)
-pr3h=subset(prec,prec[,2]==3)
-
+totales=numeric(length(fallos[,2]))
+totales=fallos[,3] + fallos[,2]
 
 pdf("${DIR_BASE}/hist_dist.pdf")
-hist(sitact[,2], main="Distacia synop petición", xlab="Distacia (m)")
+hist(sitact[,2], main="Distancia entre synop y la petición", xlab="Distacia (m)",xaxp=c(0,200,20),col="lightyellow")
 
 pdf("${DIR_BASE}/hist_tiempo.pdf")
-hist(sitact[,3], main="Tiempo synop petición", xlab="Tiempo (s)")
+hist(sitact[,3], main="Tiempo desde la petición hasta synop disponible", xlab="Tiempo (s)", breaks = seq(0, 7200, 200),xaxp=c(0,7200,36),col="lightblue")
 
 pdf("${DIR_BASE}/horas_vs_fallos.pdf")
-plot(fallos[,1], fallos[,3], type="b", xlab="Horas", ylab="# estaciones",col="blue", main="Estaciones con observaciones vs vacías")
-lines(fallos[,2],type="b",col="tomato")
-legend("topright",lty=c(1,1),col=c("blue","tomato"),c("con datos","vacías"),title="Estaciones")
-
+plot(fallos[,1], 100*fallos[,3]/totales, type="b", xlab="Horas", ylab="% estaciones",col="blue", main="Porcentajes de estaciones sin dato",xaxp=c(0,23,23))
+#lines(fallos[,2],type="b",col="tomato")
+#legend("topright",lty=c(1,1),col=c("blue","tomato"),c("con datos","vacías"),title="Estaciones")
 
 pdf("${DIR_BASE}/horas_vs_estprec.pdf")
-lim=max(prechoras[,3],prechoras[,5])
-plot(prechoras[,1],prechoras[,3],type="b", col="blue", ylim=c(0,lim), xlab="Horas", ylab="# estaciones", main="Estacion con precip. horaria y trihoraria")
-lines(prechoras[,5],type="b",col="tomato")
+lim=max(100*prechoras[,3]/prechoras[,6],100*prechoras[,5]/prechoras[,6])
+plot(prechoras[,1],100*prechoras[,3]/prechoras[,6],type="b", col="blue", ylim=c(0,lim), xlab="Horas", ylab="% estaciones", main="Estación con precip. horaria y trihoraria",xaxp=c(0,23,23))
+lines(100*prechoras[,5]/prechoras[,6],type="b",col="tomato")
 legend("topright",lty=c(1,1),col=c("blue","tomato"),c("1h","3h"))
 
 dev.off()
